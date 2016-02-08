14baaa2eb55be371dd2fde79c4ca351405eae031
Vijay Siripurapu <vsiripur@ikanos.com>
2016-02-08 16:50:06 +0530

    resolved new line issue

--- read_a_number.java
+++ read_a_number.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 public class read_a_number {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		
 		System.out.print("Please enter your name: ");
 		String name = in.nextLine();
+		
 		System.out.print("Please enter your age: ");
 		int age = in.nextInt();
+		
 		System.out.print("Please enter your mobile number: ");
 		String m_num = in.nextLine();
-		System.out.print("User name: " + name);
-		System.out.print("User age: " + age);
-		System.out.print("User mobile number: " + m_num);
+
+		System.out.println("User name: " + name);
+		System.out.println("User age: " + age);
+		System.out.println("User mobile number: " + m_num);
 	}
 }

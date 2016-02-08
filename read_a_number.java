410a33385a520a0412aa2b69661b1c88aa4fbbd7
vsiripur <vsiripur@ikanos.com>
2016-02-08 16:47:09 +0530

    removing conflicted code

--- read_a_number.java
+++ read_a_number.java
@@ -3,16 +3,13 @@ public class read_a_number {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Please enter your name: ");
-		char name = in.nextLine();
+		String name = in.nextLine();
 		System.out.print("Please enter your age: ");
 		int age = in.nextInt();
 		System.out.print("Please enter your mobile number: ");
-		char m_num = in.nextLine();
+		String m_num = in.nextLine();
 		System.out.print("User name: " + name);
 		System.out.print("User age: " + age);
-		System.out.print("User mobile number: " + num);
-		System.out.print("Please enter a number: ");
-		int num = in.nextInt();
-		System.out.println("The number you've entered is: " + num);
+		System.out.print("User mobile number: " + m_num);
 	}
 }

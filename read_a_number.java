f049df79726818668b9f9f88a8ba36d91503a98a
Vijay Siripurapu <vsiripur@ikanos.com>
2016-02-08 16:38:56 +0530

    This commit will show how to read strings and numbers from keyboard

--- read_a_number.java
+++ read_a_number.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 public class read_a_number {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Please enter a number: ");
-		int num = in.nextInt();
-		System.out.print("The number you've entered is: " + num);
+		System.out.print("Please enter your name: ");
+		char name = in.nextLine();
+		System.out.print("Please enter your age: ");
+		int age = in.nextInt();
+		System.out.print("Please enter your mobile number: ");
+		char m_num = in.nextLine();
+		System.out.print("User name: " + name);
+		System.out.print("User age: " + age);
+		System.out.print("User mobile number: " + num);
 	}
 }
